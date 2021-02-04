5466051f3a6e5cd51d311e104aa2415d5548bc2f
Ilya Novohatskyi <1llyanovohatskyy@gmail.com>
2021-02-04 20:04:50 +0200

    New design for Account Balance

--- src/components/AccountBalance/index.tsx
+++ src/components/AccountBalance/index.tsx
@@ -1,64 +1,342 @@
-import React, { FC, useEffect, useState } from 'react'
-import { useFetchListCallback } from 'hooks/useFetchListCallback'
-import styles from './styles.module.css'
-import { TokenInfo } from '@uniswap/token-lists'
+/* eslint-disable */
+import React, { useEffect, useState } from 'react';
+import { Button, createStyles, FormControlLabel, lighten, makeStyles, Paper, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TableSortLabel, Theme, Toolbar, Typography } from '@material-ui/core';
+import clsx from 'clsx';
 
-interface Props {
-  url?: string | undefined
+interface Data {
+  img: string;
+  coin: string;
+  balance: number;
+  available: number;
+  borrowed: number;
+  ir: number;
 }
 
-const AccountBalance: FC<Props> = ({ url }: Props) => {
-  const [tokens, setTokens] = useState<TokenInfo[]>([])
-  const fetchList = useFetchListCallback()
+function createData(
+  img: string,
+  coin: string,
+  balance: number,
+  available: number,
+  borrowed: number,
+  ir: number,
+): Data {
+  return {
+    img,
+    coin,
+    balance,
+    available,
+    borrowed,
+    ir
+  };
+}
 
-  useEffect(() => {
-    if (url) {
-      fetchList(url, false)
-        .then(({ tokens }) => setTokens(tokens))
-        .catch(error => console.debug('interval list fetching error', error))
-    }
-    // eslint-disable-next-line
-  }, [url])
 
-  console.log(tokens)
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = 'asc' | 'desc';
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof Data;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: HeadCell[] = [
+  { id: 'img', numeric: false, disablePadding: true, label: '' },
+  { id: 'coin', numeric: false, disablePadding: true, label: 'Coin' },
+  { id: 'balance', numeric: true, disablePadding: false, label: 'Total Balance' },
+  { id: 'available', numeric: true, disablePadding: false, label: 'Available' },
+  { id: 'borrowed', numeric: true, disablePadding: false, label: 'Borrowed' },
+  { id: 'ir', numeric: true, disablePadding: false, label: 'Interest Rate' },
+];
+
+interface EnhancedTableProps {
+  classes: ReturnType<typeof useStyles>;
+  numSelected: number;
+  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { classes, order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+    onRequestSort(event, property);
+  };
 
   return (
-    <div className={styles.balanceWrapper}>
-      <div className={styles.tokenItem}>
-        <h3>Token Name</h3>
-        <div style={{ display: 'flex', justifyContent: 'space-between', width: '50%' }}>
-          <div className={styles.tooltip}>
-            IR
-            <span className={styles.tooltiptext}>
-              The Annual Interest Rate is the rate of growth of your asset if you allowed the asset to be deposited on
-              the platform for a year. Interest rates are subject to fluctuations, either modified by the platform owner
-              or by supply-demand mechanics of users interacting with the platform.
-            </span>
-          </div>
-          <div>Balance</div>
-        </div>
-      </div>
-      {tokens.length &&
-        tokens.map(token => (
-          <div className={styles.tokenItem} key={token.chainId + token.address}>
-            <div style={{ display: 'flex' }}>
-              <img height={24} width={24} src={token.logoURI} alt={token.symbol} />
-              <div className={styles.nameDiv}>
-                <h3>{token.symbol}</h3>
-                <h6>{token.name}</h6>
-              </div>
-            </div>
-            <div style={{ display: 'flex', width: '50%', justifyContent: 'space-between' }}>
-              <div>0.00%</div>
-              <div className={styles.balance}>0.00</div>
-            </div>
-          </div>
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
         ))}
-    </div>
-  )
-}
-AccountBalance.defaultProps = {
-  url: undefined
+        <TableCell padding="default" align="right">
+          Actions
+         </TableCell>
+      </TableRow>
+    </TableHead>
+  );
 }
 
-export default AccountBalance
+const useToolbarStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(1),
+    },
+    highlight:
+      theme.palette.type === 'light'
+        ? {
+          color: theme.palette.secondary.main,
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+        }
+        : {
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.secondary.dark,
+        },
+    title: {
+      flex: '1 1 100%',
+    },
+  }),
+);
+
+interface EnhancedTableToolbarProps {
+  numSelected: number;
+}
+
+const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
+  const classes = useToolbarStyles();
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      className={clsx(classes.root, {
+        [classes.highlight]: numSelected > 0,
+      })}
+    >
+      {numSelected > 0 ? (
+        <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
+          {numSelected} selected
+        </Typography>
+      ) : (
+          <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+            Account Balance
+          </Typography>
+        )}
+    </Toolbar>
+  );
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+    },
+    paper: {
+      width: '100%',
+      marginBottom: theme.spacing(2),
+    },
+    table: {
+      minWidth: 750,
+    },
+    visuallyHidden: {
+      border: 0,
+      clip: 'rect(0 0 0 0)',
+      height: 1,
+      margin: -1,
+      overflow: 'hidden',
+      padding: 0,
+      position: 'absolute',
+      top: 20,
+      width: 1,
+    },
+  }),
+);
+
+export default function EnhancedTable({ tokens }: any) {
+  const classes = useStyles();
+  const [order, setOrder] = useState<Order>('asc');
+  const [orderBy, setOrderBy] = useState<keyof Data>('coin');
+  const [selected] = useState<string[]>([]);
+  const [page, setPage] = useState(0);
+  const [dense, setDense] = useState(false);
+  const [rowsPerPage, setRowsPerPage] = useState(25);
+  const [rows, setRows] = useState([createData('', '', 0, 0, 0, 0)]);
+
+  useEffect(() => {
+    const unique: string[] = [];
+    const newTokens = tokens
+      .filter(({ symbol, logoURI }: any) => {
+        if (!unique.includes(symbol) && logoURI) {
+          unique.push(symbol);
+          return true;
+        }
+        return false
+      })
+      .map(({ logoURI, symbol }: any) => createData(logoURI, symbol, Math.random(), Math.random(), Math.random(), Math.random()))
+    setRows(newTokens)
+  }, [tokens])
+
+  const handleRequestSort = (event: React.MouseEvent<unknown>, property: keyof Data) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected: string[] = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    // setSelected(newSelected);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = (name: string) => selected.indexOf(name) !== -1;
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table
+            className={classes.table}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+            aria-label="enhanced table"
+          >
+            <EnhancedTableHead
+              classes={classes}
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.coin as string);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.coin)}
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.borrowed}
+                      selected={isItemSelected}
+                    >
+                      <TableCell padding="checkbox" align="right">
+                        <img src={row.img} alt={row.coin} height={24} />
+                      </TableCell>
+                      <TableCell id={labelId} align="left">{row.coin}</TableCell>
+                      <TableCell align="right">{row.balance.toFixed(6)}</TableCell>
+                      <TableCell align="right">{row.available.toFixed(6)}</TableCell>
+                      <TableCell align="right" padding="none">
+                        {row.borrowed.toFixed(6)}
+                      </TableCell>
+                      <TableCell align="right">{row.ir.toFixed(6)}</TableCell>
+                      <TableCell align="right">
+                        <Button variant="text" style={{ textTransform: 'none' }} color="primary" onClick={e => e.preventDefault()}>Borrow/Repay</Button>
+                        <Button variant="text" style={{ textTransform: 'none' }} color="primary" onClick={e => e.preventDefault()}>Withdraw</Button>
+                        <Button variant="text" style={{ textTransform: 'none' }} color="primary" onClick={e => e.preventDefault()}>Deposit</Button>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 50, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Hide zero balances (not implemented)"
+      />
+    </div>
+  );
+}
