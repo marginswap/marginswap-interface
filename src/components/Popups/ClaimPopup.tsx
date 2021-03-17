2b4523b18184a8cb1d1978089b9c61a497c57bb9
Gabriel Pickard <wergomat@gmail.com>
2021-03-17 10:08:04 -0500

    change everything to marginswap-sdk

--- src/components/Popups/ClaimPopup.tsx
+++ src/components/Popups/ClaimPopup.tsx
@@ -1,4 +1,4 @@
-import { TokenAmount } from '@uniswap/sdk'
+import { TokenAmount } from 'marginswap-sdk'
 import React, { useEffect } from 'react'
 import { X } from 'react-feather'
 import styled, { keyframes } from 'styled-components'
