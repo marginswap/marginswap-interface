49ffa2b3faef34950a7ccccbd906077b328b2c7b
Koochr <dm.girenko@gmail.com>
2021-04-13 16:40:37 +0300

    Fix wallet logos

--- src/components/WalletModal/index.tsx
+++ src/components/WalletModal/index.tsx
@@ -281,7 +281,7 @@ export default function WalletModal({
             link={option.href}
             header={option.name}
             subheader={null} //use option.descriptio to bring back multi-line
-            icon={require('../../assets/images/' + option.iconName)}
+            icon={'/images/' + option.iconName}
           />
         )
       )
