1a2dbcdfb730a9cd2ac9044577fc6e393af28fc0
webguru2 <poohstar27@gmail.com>
2021-04-13 03:04:41 +0200

    update swap footer details

--- src/components/swap/AdvancedSwapDetails.tsx
+++ src/components/swap/AdvancedSwapDetails.tsx
@@ -102,16 +102,6 @@ export function AdvancedSwapDetails({ trade }: AdvancedSwapDetailsProps) {
               </RowBetween>
             </>
           )}
-          {!showRoute && (
-            <AutoColumn style={{ padding: '12px 16px 0 16px' }}>
-              <InfoLink
-                href={'https://uniswap.info/pair/' + trade.route.pairs[0].liquidityToken.address}
-                target="_blank"
-              >
-                View pair analytics ↗
-              </InfoLink>
-            </AutoColumn>
-          )}
         </>
       )}
     </AutoColumn>
