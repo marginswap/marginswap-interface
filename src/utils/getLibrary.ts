96c7b05cac758cc632572c9837a0b066ccf7f6e5
Gabriel Pickard <wergomat@gmail.com>
2021-06-09 04:03:18 -0500

    even super slower

--- src/utils/getLibrary.ts
+++ src/utils/getLibrary.ts
@@ -2,6 +2,6 @@ import { Web3Provider } from '@ethersproject/providers'
 
 export default function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider, 'any')
-  library.pollingInterval = 100000
+  library.pollingInterval = 6000000
   return library
 }
